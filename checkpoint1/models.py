b46838f677bc87c38676fdae9115f58a09da5be3
olufunmiladeo <tyos137@gmail.com>
2015-10-05 01:29:36 +0100

    Allocation models for Amity

--- checkpoint1/models.py
+++ checkpoint1/models.py
@@ -2,68 +2,86 @@
 A room allocation system for Amity.
 By Olufunmilade Oshodi.
 
-Model file for allocation in Amity
+Allocation Models for Amity
 '''
 
 
-class Space(object):
-    max_size = 0
+class Room(object):
 
     def __init__(self, name):
+        '''
+        Initialize an instance
+        Takes a string argument, name
+        '''
         self.name = name
+        self.members = []
+
+    def get_members(self):
+        '''
+        Returns a list of members allocated to a room
+        '''
+        return self.members
+
+    def current_size(self):
+        '''
+        Returns the current size of a room
+        '''
+        return len(self.members)
+
+    def add_member(self, member):
+        '''
+        Adds a member to a room
+        '''
+        try:
+            if " #".join([member.name, member.gender]) not in self.members:
+                self.members.append(" #".join([member.name, member.gender]))
+        except AttributeError:
+            print "Can't determine if {} is".format(member),
+            print "a 'fellow' or 'staff'"
 
 
-class Office(Space):
+class Office(Room):
     max_size = 6
-    office_dic = {}
-
-    def set_room(self):
-        self.office_dic[self.name] = []
-
-    def get_members(self):
-        return self.office_dic[self.name]
-
-    def current_size(self):
-        return len(self.office_dic[self.name])
+    room_type = 'office'
+    designation = 'unisex'
 
 
-class Living(Space):
+class Living(Room):
     max_size = 4
-    living_dic = {}
+    room_type = 'living'
 
-    def set_room(self):
-        self.living_dic[self.name] = []
-
-    def get_members(self):
-        return self.living_dic[self.name]
-
-    def current_size(self):
-        return len(self.living_dic[self.name])
+    def __init__(self, name, designation):
+        '''
+        Initialize an instance
+        Takes two string arguments, name and designation = "male" or "female"
+        '''
+        self.designation = designation
+        self.designation = self.designation.lower()
+        if self.designation == "male" or self.designation == "female":
+            super(Living, self).__init__(name)
+        else:
+            raise ValueError(
+                "Please designate the room as either male or female")
 
 
 class Person(object):
 
-    def __init__(self, name):
+    def __init__(self, name, gender):
+        '''
+        Initialize an instance
+        Takes two string arguments, name and gender = "M" or "F"
+        '''
         self.name = name
+        self.gender = gender
+        self.gender = self.gender.upper()
+        if self.gender != "M" and self.gender != "F":
+            raise ValueError("Please specify the gender as either 'M' or 'F'")
 
 
 class Staff(Person):
-    staff_list = []
-
-    def set_staff(self):
-        self.staff_list.append(self.name)
+    pass
 
 
 class Fellow(Person):
-    male_list = []  # list for allocation to male rooms
-    female_list = []  # list for allocation to female rooms
-    mixed_list = []  # list for no allocation to living space
-
-    def set_male(self):
-        self.male_list.append(self.name)
-
-    def set_female(self):
-        self.female_list.append(self.name)
-
-    def set_mixed(self):
-        self.mixed_list.append(self.name)
+    choice = ''
+    pass
